a7aca911e85a0ea0e5e84e5320de9b2a9e747df1
James <j.a.pratt@outlook.com>
2021-01-17 13:37:27 +0000

    joinNames Complete

--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,9 @@
 const { joinNames } = require("../src");
-
+const simpsons = [{name: "Bart"}, {name: "Lisa"}, {name: "Maggie"}];
+const familyDogs = [{name: "Daisy"}, {name: "Darwin"}, {name: "Millie"}, {name: "Maddy"},{name: "Max"}];
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames(simpsons)).toStrictEqual("Bart, Lisa & Maggie");
+   expect(joinNames(familyDogs)).toStrictEqual("Daisy, Darwin, Millie, Maddy & Max");
+  });
 });
