9bb98f073c727b7b62df21d67cfac0063fca6352
James <j.a.pratt@outlook.com>
2021-01-17 19:17:14 +0000

    getEmployer 'role' is undifined

--- test/kata7.getEmployerRole.test.js
+++ test/kata7.getEmployerRole.test.js
@@ -1,5 +1,11 @@
 const { getEmployerRole } = require("../src");
+const employees = [{name: "Gandalf", role: "Wizzard"},{name: "Aragorn", role: "Ranger of the North"},{name: "Gimli", role:"Dwarf Warrior"}, {name: "Sauron", role: "The Dark Lord of Mordor"}];
 
 describe("getEmployerRole", () => {
-  test("returns the employee's role in the company", () => {});
+  test("returns the employee's role in the company", () => {
+    expect(getEmployerRole("Gadalf", employees)).toEqual("Wizzard");
+    expect(getEmployerRole("Aragorn", employees)).toEqual("Ranger of the North")
+    expect(getEmployerRole("Sauron", employees)).toEqual("The Dark Lord of Mordor"); 
+    expect(getEmployerRole("Gimli", employees)).toEqual("Dwarf Warrior"); 
+  });
 });
