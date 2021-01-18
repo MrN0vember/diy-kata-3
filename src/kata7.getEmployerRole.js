4899037f01d245fecbf5c0e35ee7c3ad187287ac
James <j.a.pratt@outlook.com>
2021-01-18 21:05:00 +0000

    getEmployerRole complete

--- src/kata7.getEmployerRole.js
+++ src/kata7.getEmployerRole.js
@@ -2,7 +2,7 @@ const getEmployerRole = (employeeName, employees) => {
     const employee = employees.filter(employee => employee.name === employeeName)
     const roleInCompany = employee[0];
    return roleInCompany.role;
-   
+
 };
 
 
